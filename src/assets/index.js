6c0e3b3218922e1bc3e78167108fdf97ceeaaeb0
Fakhri Azmi Husaini <rifakhrii@gmail.com>
2023-09-05 09:39:33 +0700

    delete image

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,14 +1,6 @@
 //Icon
-import IconNote from './icons/IconNote.svg'
 import IconAdd from './icons/IconAdd.svg'
 
-//Image
-import ImageWelcome from './images/ImageWelcome.png'
-
 export {
-    IconNote,
-    IconAdd,
-
-
-    ImageWelcome
+    IconAdd
 }
